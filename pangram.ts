bbddc3c3f91bf924e6483c500e6e9b1ebfa91028
Kyle Coberly <kyle.coberly@gmail.com>
2022-04-27 18:13:52 -0600

    Add solution, remove tests

--- pangram.ts
+++ pangram.ts
@@ -1,1 +1,22 @@
-export function isPangram() {}
+const aToZ = [...Array<string>(26)].map((_, index) => {
+  return String.fromCharCode(index + 65);
+});
+
+export function isPangram(value: string): boolean {
+  const myMap = new Map<string, number>();
+  aToZ.forEach((key: string) => {
+    myMap.set(key.toLowerCase(), 0);
+  });
+
+  for (const each of value) {
+    const item = myMap.get(each) || 0;
+    myMap.set(each.toLowerCase(), item + 1);
+  }
+
+  for (const each of myMap.values()) {
+    if (each === 0) {
+      return false;
+    }
+  }
+  return true;
+}
